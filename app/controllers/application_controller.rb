644fac18677bbfeb9f74c21eb51583bfb585c8fd
kenshin812 <t21061007@gmail.com>
2025-09-14 22:33:48 +0900

    Basic認証用

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  before_action :basic_auth
+
+  private
+
+  # このメソッドを追加
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      # credentialsからユーザー名とパスワードを読み込んで比較
+      username == Rails.application.credentials.production[:basic_auth_username] &&
+      password == Rails.application.credentials.production[:basic_auth_password]
+    end
+  end
 end
